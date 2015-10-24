50e5f20d73e235cc342c7aad1fa50b351a13f068
guand <guan3d@gmail.com>
2015-10-23 18:29:53 -0700

    Added movement to the wings that sync with the body

--- animation/main.cpp
+++ animation/main.cpp
@@ -25,25 +25,32 @@ void display(){
 
     float time_s = glfwGetTime();
 
+//    float freq = M_PI*time_s*SPEED_FACTOR;
+    float wingFlap = std::sin(time_s);
 
     //TODO: Set up the transform hierarchies for the three objects!
     Transform head_M = Transform::Identity();
-    head_M *= Eigen::Translation3f(0.0, 1.5, 0.0);
+    head_M *= Eigen::Translation3f(0.0, 1.5 + wingFlap * 0.75, 0.0);
     head_M *= Eigen::AlignedScaling3f(.5, .5, 1.0);
 
     head.draw(head_M.matrix());
 
     Transform body_M = Transform::Identity();
+    body_M *= Eigen::Translation3f(0.0, 0.0 + wingFlap * 0.75, 0.0);
     body.draw(body_M.matrix());
     Transform wingR_M = Transform::Identity();
-    wingR_M *= Eigen::Translation3f(1.5, 1.0, 0.0);
-    wingR_M *= Eigen::AlignedScaling3f(.5, .5, 1.0);
+    wingR_M *= Eigen::AlignedScaling3f(.75, .75, 1.0);
+    wingR_M *= Eigen::Translation3f(1.3, 1.33 + wingFlap, 0.0);
     wingR_M *= Eigen::Affine3f(Eigen::AngleAxisf(M_PI/2.0, vec3::UnitZ())).matrix();
+    wingR_M *= Eigen::Affine3f(Eigen::AngleAxisf(wingFlap, vec3::UnitZ())).matrix();
+    wingR_M *= Eigen::Translation3f(0.0, -1.0, 0.0);
     wingR.draw(wingR_M.matrix());
     Transform wingL_M = Transform::Identity();
-    wingL_M *= Eigen::Translation3f(-1.5, 1.0, 0.0);
-    wingL_M *= Eigen::AlignedScaling3f(.5, .5, 1.0);
+    wingL_M *= Eigen::AlignedScaling3f(.75, .75, 1.0);
+    wingL_M *= Eigen::Translation3f(-1.3, 1.33 + wingFlap, 0.0);
     wingL_M *= Eigen::Affine3f(Eigen::AngleAxisf(M_PI/-2.0, vec3::UnitZ())).matrix();
+    wingL_M *= Eigen::Affine3f(Eigen::AngleAxisf(-wingFlap, vec3::UnitZ())).matrix();
+    wingL_M *= Eigen::Translation3f(0.0, -1.0, 0.0);;
     wingL.draw(wingL_M.matrix());
 
 }
